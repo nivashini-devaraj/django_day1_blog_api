af4fd87a89c87917b5ddc12cc14fbf863a4471d3
nivashini-devaraj <nivashinid@gmail.com>
2025-12-30 20:26:56 +0530

    Added html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BlogViewSet
+from .views import blog_list
+from .views import blog_delete
+from .views import blog_edit
+from .views import blog_create
 
 router = DefaultRouter()
 router.register(r'blogs', BlogViewSet, basename='blog')
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('blogs-page/', blog_list, name='blog-list-page'),
+    path('blogs/delete/<int:id>/', blog_delete, name='blog-delete-page'),
+    path('blogs/edit/<int:id>/', blog_edit, name='blog-edit-page'),
+    path('blog-create/', blog_create, name='blog-create-page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,44 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Blog
 from .serializers import BlogSerializer   
+from django.shortcuts import render
+from .models import Blog
+from django.shortcuts import redirect, get_object_or_404
+
 
 class BlogViewSet(ModelViewSet):
     queryset = Blog.objects.all()         
     serializer_class = BlogSerializer 
+
+def blog_create(request):
+    if request.method == 'POST':
+        Blog.objects.create(
+            title=request.POST['title'],
+            content=request.POST['content'],
+            author=request.user   
+        )
+        return redirect('blog-list-page')
+
+    return render(request, 'blog/blog_create.html')
+
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/blog_list.html', {'blogs': blogs})
+
+def blog_edit(request, id):
+    blog = get_object_or_404(Blog, id=id)
+
+    if request.method == 'POST':
+        blog.title = request.POST['title']
+        blog.content = request.POST['content']
+        blog.save()
+        return redirect('blog-list-page')
+
+    return render(request, 'blog/blog_edit.html', {'blog': blog})
+
+
+def blog_delete(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    blog.delete()
+    return redirect('blog-list-page')
